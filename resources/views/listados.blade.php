e047b92499f85c080732f0a6b82e49e6af8f8480
xabilazkano <xlazcanoit18dw@ikzubirimanteo.com>
2019-12-11 21:18:10 +0100

    Ejercicio 10 terminado

---
+++ resources/views/listados.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app2')
+@section('content')
+<div class="row">
+	<div class="col-3">
+		<div class="listado p-5 border">
+			<h2>Listados</h2>
+			<ul class="nav flex-column">
+				<li class="nav-item">
+					<a class="nav-link" href="#">Link</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Link</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Link</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
+@endsection
